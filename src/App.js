457636bd59c0b3e5549754eeb22b3fecd71521b6
Harshit <harshitrichariya77@gmail.com>
2020-07-30 17:12:11 +0530

    Initial commit
    Added latest photos functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Image from "./components/Image";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      images: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://api.unsplash.com/photos/?client_id=LqP1bNdvX8VncEmSaVp23fXv_ZzZf-drxT4V0cS1eGM&page=1&per_page=30&order_by=latest"
+    )
+      .then((res) => res.json())
+      .then(
+        (json) => {
+          this.setState({
+            isLoaded: true,
+            images: json,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
+  }
+
+  render() {
+    const { error, isLoaded, images } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="App-header">
+          {images.map((image) => (
+            <Image image={image} key={image.id} />
+          ))}
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
